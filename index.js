548c370808a04d6ffa9ab2da5e45aa0148113fb8
Lenuam Zambrano <pepeitopepote3@gmail.com>
2026-04-04 09:56:09 -0500

    changes

--- index.js
+++ index.js
@@ -17,17 +17,67 @@
 //  : "Excellent"
 // console.log(message)
 
-const playerGuess = 3
-const correctAnswer = 6
-console.log("hola")
+// const playerGuess = 3
+// const correctAnswer = 6
+// console.log("hola")
 
 
-const message = playerGuess === correctAnswer ? "correct"
-: playerGuess > correctAnswer ? "your guess is to high"
-:"your guess is to low"
-// if(playerGuess === correctAnswer){
-//     message = "correct"
-// }else{
-//     message = "wrong"
+// const message = playerGuess === correctAnswer ? "correct"
+// : playerGuess > correctAnswer ? "your guess is to high"
+// :"your guess is to low"
+// // if(playerGuess === correctAnswer){
+// //     message = "correct"
+// // }else{
+// //     message = "wrong"
+// // }
+// console.log(message)
+
+// function selectItem(item){
+//     let price = 0
+//     switch(item){
+//         case "coffee":
+//             price = 2
+//             break
+//         case "sandwich":
+//             price = 5
+//             break
+//         case "salad":
+//             price = 4
+//             break
+//         case "Lemon cake":
+//             price = 3
+//             break
+//         default:
+//             return `Sorry, we dont sell ${item}`
+//     }
+//     return `you've selected ${item} and the price is $${price}`
 // }
-console.log(message)
+// // console.log(selectItem("sandwich"))
+
+// const dreamholiday={
+//     destination: 'dubay',
+//     activity: 'visit the tallest building in the world',
+//     accomodation: 'i would get into a luxury hotel',
+//     companion: "alone"
+// }
+
+// const{destination,activity,accomodation,companion} = dreamholiday
+
+// console.log(`I would love going to ${destination} to ${activity}
+// then ${accomodation} and i would go completely ${companion}`
+// )
+
+const correctAnswerInfo = {
+    capital: "lima",
+    points: 10 + " points"
+}
+const{capital,points} = correctAnswerInfo
+
+console.log("what is the capital of peru")
+
+setTimeout(logAnswer,3000,capital)
+
+
+function logAnswer(ans){
+    console.log(`the answer is ${ans} of course! If you got that right, give yourself ${points}`)
+}
